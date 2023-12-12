413da652bb16d533675c211e664701e1a843a6c7
HugoPhi <hugonelson07@outlook.com>
2023-12-12 19:36:24 +0800

    commit by hugo

--- compile.sh
+++ compile.sh
@@ -5,10 +5,9 @@ input_file="VMakeList.txt"
 output_file="top.vvp"
 
 sed "s|\$prj|${prj}|g" ${prj}/${input_file} > ${prj}/temp_file_list.txt
-iverilog -c ${prj}/temp_file_list.txt -o ${prj}/target/${output_file}
+iverilog -c ${prj}/temp_file_list.txt -o ${prj}/target/${output_file} 
 
 rm ${prj}/temp_file_list.txt
 
-# iverilog -o ${item_path}/target/top.vvp -c ${item_path}/VMakeList.txt
 
 
--- src/convdim1.v
+++ src/convdim1.v
@@ -6,12 +6,16 @@ module convdim1 #(parameter inlen=255, parameter kernlen=3) (
     // 1-dim convolution
     reg[inlen+kernlen-2:0]             conv_res;
     reg[inlen+2*(kernlen-1):0]         augmented_in;
-   
-    
-    always @(*) begin
+
+    // data preparation
+    initial begin
         conv_res = 0;
         augmented_in = 0;
-        augmented_in[kernlen-1+inlen:kernlen-1] = in;
+        augmented_in[kernlen-1+inlen:kernlen-1] = in;  // pad with 0 
+    end
+    
+    /// serial implementation
+    always @(*) begin
         for (integer i = 0; i < inlen + kernlen - 1; i = i + 1) begin
             for (integer j = 0; j < kernlen; j = j + 1) begin
                 conv_res[i] = conv_res[i] + augmented_in[i+j] * kernel[kernlen-1-j];
@@ -20,6 +24,17 @@ module convdim1 #(parameter inlen=255, parameter kernlen=3) (
         end
     end
     
-    assign out = conv_res;
+    /// parallel implementation
+    // always @(*) begin
+    //     generate 
+    //         for (integer i = 0; i < inlen + kernlen - 1; i = i + 1): generate_loop
+    //             for (integer j = 0; j < kernlen; j = j + 1) begin
+    //                 conv_res[i] = conv_res[i] + augmented_in[i+j] * kernel[kernlen-1-j];
+    //                 // conv_res[i] = conv_res[i] | (augmented_in[i+j] & kernel[kernlen-1-j]);
+    //             end
+    //         end
+    //     endgenerate
+    // end
     
+    assign out = conv_res;
 endmodule
